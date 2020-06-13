c29c73a1c06c55ce44fefbaf71abee967df1d903
kemeng1414 <524277450@qq.com>
2020-06-13 17:46:39 +0800

    '添加表结构设计'

--- scripts/init_user.py
+++ scripts/init_user.py
@@ -9,3 +9,5 @@ django.setup()
 
 from web import models
 models.UserInfo.objects.create(username='alex',email='55252663@qq.com',mobile_phone='13025252525',password='12345678')
+
+models.PriceInfo.objects.create()
